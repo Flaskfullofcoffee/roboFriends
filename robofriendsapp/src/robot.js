1b2e4b1f9d85668f1fa110db63c307dd1421b7c4
Zac <zdobbs3@gmail.com>
2018-09-23 12:18:22 -0700

    set styles for cards & touched CardList.js

---
+++ robofriendsapp/src/robot.js
@@ -0,0 +1,38 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Jacob nickerbokr',
+    username: 'jaybokr',
+    email: 'jaybokr@mail.com'
+  },
+  {
+    id: 2,
+    name: 'Dan Bolts',
+    username: 'dbolt',
+    email: 'dbolt@mail.com'
+  },
+  {
+    id: 3,
+    name: 'Suzy Que',
+    username: 'suzy',
+    email: 'suzyq@mail.com'
+  },
+  {
+    id: 4,
+    name: 'Beth Roads',
+    username: 'Beth',
+    email: 'broads@mail.com'
+  },
+  {
+    id: 5,
+    name: 'Fred Jaxson',
+    username: 'Jax',
+    email: 'fjaxson@mail.com'
+  },
+  {
+    id: 6,
+    name: 'Cooper Loeads',
+    username: 'Coop',
+    email: 'coopl@mail.com'
+  }
+]
