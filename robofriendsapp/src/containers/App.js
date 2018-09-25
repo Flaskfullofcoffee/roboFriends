3644f3cb1763bb12c2c071c95464b320a1fca93f
Zac <zdobbs3@gmail.com>
2018-09-25 13:57:02 -0700

    added sandbox.js

--- robofriendsapp/src/containers/App.js
+++ robofriendsapp/src/containers/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox'
-// import { robots } from './robot.js'
-import Scroll from './Scroll.js'
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox'
+import Scroll from '../components/Scroll.js'
 
 
 class App extends Component {
@@ -25,13 +24,13 @@ onSearchChange = (event) => {
 }
 
   render() {
-  const filteredRobots = this.state.robots.filter((robots) => {
-    return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    const { robots, searchfield } = this.state;
+  const filteredRobots = robots.filter((robot) => {
+    return robot.name.toLowerCase().includes(searchfield.toLowerCase());
   })
-  if (this.state.robots.length === 0) {
-    return <h1 className='f1 tc'>LOADING...</h1>
-  } else {
-    return(
+  return !robots.length ?
+     <h1 className='f1 tc'>LOADING...</h1> :
+     (
       <div className='tc'>
         <h1 className='tc f1'>Robo Friends</h1>
         <SearchBox searchChange={this.onSearchChange}/>
@@ -40,7 +39,6 @@ onSearchChange = (event) => {
         </Scroll>
       </div>
     );
-  }
   }
 }
 
